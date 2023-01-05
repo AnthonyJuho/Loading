d89ed58229ffa5251639c5c304db6e0c886e5500
AnthonyJuho <112909624+AnthonyJuho@users.noreply.github.com>
2023-01-06 00:08:16 +0900

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,23 @@
+var circle = document.getElementById("circle");
+var loading = document.getElementById("loading");
+
+var red = 0;
+var green = 0;
+var blue = 0;
+var angle = 0;
+
+setInterval(() => {
+    angle += 1;  
+    if (angle<60) {red = 255; green = Math.round(angle*4.25-0.01); blue = 0;} else
+    if (angle<120) {red = Math.round((120-angle)*4.25-0.01); green = 255; blue = 0;} else 
+    if (angle<180) {red = 0, green = 255; blue = Math.round((angle-120)*4.25-0.01);} else 
+    if (angle<240) {red = 0, green = Math.round((240-angle)*4.25-0.01); blue = 255;} else 
+    if (angle<300) {red = Math.round((angle-240)*4.25-0.01), green = 0; blue = 255;} else 
+    {red = 255, green = 0; blue = Math.round((360-angle)*4.25-0.01);}
+    if(angle == 360) {
+        angle = 0;
+    }
+    circle.style.borderTopColor = "rgb("+red+","+green+","+blue+")";
+    circle.style.transform = "rotate("+angle+"deg)";
+    loading.style.color = "rgb("+red+","+green+","+blue+")";
+}, 10);
